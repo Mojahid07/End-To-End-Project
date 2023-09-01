15f14407bb74deb3b33d1ab2a4b5c572a902bba0
Sayad Mojahid <sayadmojahid07@gmail.com>
2023-09-01 14:16:42 +0530

    setup commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='sayad mojahid',
+    version='',
+    author='Mojahid Sayad',
+    author_email='sayadmojahid07@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy'],
+)
